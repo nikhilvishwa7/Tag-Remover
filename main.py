288a6612332817043e6cf92eff9d68d96b375b04
nikhilvishwa7 <123230101+nikhilvishwa7@users.noreply.github.com>
2023-05-20 14:59:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ bughunter0 = Client(
 
 @bughunter0.on_message(filters.command(["start"]))
 async def start(bot, update):
-    await update.reply_text("Join My Update Channel")
-href=https://t.me/24x7Update
+    await update.reply_text("Join My Update Channel : @24x7Update")
+
    
 @bughunter0.on_message(filters.forwarded and filters.channel)
 async def channeltag(bot, message):
